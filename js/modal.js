72ac239eb2fe9fda6f9e40c728017bb6b01592fd
Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2023-11-17 13:52:07 +0200

    Add complete responsive layout, slider in formats

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('#modal-btn-open');
+const modalHeroBtnOpen = document.querySelector('#modal-hero-btn-open');
 const modalBtnClose = document.querySelector('#modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
+modalHeroBtnOpen.addEventListener('click', toggleModal);
