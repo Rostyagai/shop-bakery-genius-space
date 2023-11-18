b8651a249a7ded8b8fcba1a05f095d3b4fa68a52
Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2023-11-18 14:44:58 +0200

    Add retinization, mobile menu and more
    1. Add full website retinization for most images;
    2. Add mobile menu;
    3. Add decorations to the modal/menu;
    4. Change images for different screens;
    5. Fix some adaptive issues;
    6. "Squoosh" images optimization

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,13 @@
 const mobileMenu = document.querySelector('#mobile-menu');
 const menuBtnOpen = document.querySelector('#menu-btn-open');
 const menuBtnClose = document.querySelector('#menu-btn-close');
+const menuLink = document.querySelectorAll('.mobile-menu__link');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+menuLink[0].addEventListener('click', toggleMenu);
+menuLink[1].addEventListener('click', toggleMenu);
+menuLink[2].addEventListener('click', toggleMenu);
+menuLink[3].addEventListener('click', toggleMenu);
