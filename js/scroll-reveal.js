af43f76c039d209ab35f46a45518a79f328e68d2
Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2023-11-28 17:11:50 +0200

    Fix scroll bug, hero img bug

---
+++ js/scroll-reveal.js
@@ -0,0 +1,7 @@
+ScrollReveal({
+  delay: 350,
+  duration: 2000,
+  distance: '30px',
+  
+});
+ScrollReveal().reveal('.hero-section__title, .section, .section__title, .traditions-section__photo, .advantages-section, .hero-section__button');
